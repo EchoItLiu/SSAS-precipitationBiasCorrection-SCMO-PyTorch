dc8032b3f6e2e3c0195f98116c726324dbe66f83
Louie <30547036+EchoItLiu@users.noreply.github.com>
2021-03-16 00:42:02 +0800

    Add files via upload

---
+++ preprocessing/pre_ERA5/TS_ERA5/PreERA5_ts_modals_ec.sh
@@ -0,0 +1,10 @@
+# 6-null
+# nohup python PreERA5_ts_modals_ec.py 2019060100020190701 > 20190601.out 2>&1
+# nohup python PreERA5_ts_modals_ec.py 2019070100020190801 > 20190701.out 2>&1
+nohup python PreERA5_ts_modals_ec.py 2019080100020190901 > 20190801.out 2>&1
+nohup python PreERA5_ts_modals_ec.py 2019090100020191001 > 20190901.out 2>&1
+nohup python PreERA5_ts_modals_ec.py 2019100100020191101 > 20191001.out 2>&1
+nohup python PreERA5_ts_modals_ec.py 2019110100020191201 > 20191101.out 2>&1
+# nohup python PreERA5_ts_modals_ec.py 2019090100020191001 > 20190901.out 2>&1
+# nohup python PreERA5_ts_modals_ec.py 2019100100020191101 > 20191001.out 2>&1
+# nohup python PreERA5_ts_modals_ec.py 2019110100020191201 > 20191101.out 2>&1
