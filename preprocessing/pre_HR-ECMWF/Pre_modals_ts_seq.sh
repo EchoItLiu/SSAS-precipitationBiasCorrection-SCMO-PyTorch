2951b86a952c92c449e28272b5f5d8493c68f1fc
Louie <30547036+EchoItLiu@users.noreply.github.com>
2021-03-16 00:47:18 +0800

    Add files via upload

---
+++ preprocessing/pre_HR-ECMWF/Pre_modals_ts_seq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+nohup python pre_modals_ts.py 20160701 > 20160701.out 2>&1
+nohup python pre_modals_ts.py 20160801 > 20160801.out 2>&1
+nohup python pre_modals_ts.py 20160901 > 20160901.out 2>&1
+# nohup python pre_modals_ts.py 20161001 > 20161001.out 2>&1
+# nohup python pre_modals_ts.py 20161101 > 20161101.out 2>&1
+# nohup python pre_modals_ts.py 20161201 > 20161201.out 2>&1
+# nohup python pre_modals_ts.py 20170401 > 20170401.out 2>&1
+# nohup python pre_modals_ts.py 20170501 > 20170501.out 2>&1
+# nohup python pre_modals_ts.py 20170601 > 20170601.out 2>&1
+nohup python pre_modals_ts.py 20170701 > 20170701.out 2>&1
+nohup python pre_modals_ts.py 20170801 > 20170801.out 2>&1
+nohup python pre_modals_ts.py 20170901 > 20170901.out 2>&1
+# nohup python pre_modals_ts.py 20171001 > 20171001.out 2>&1
+
+
